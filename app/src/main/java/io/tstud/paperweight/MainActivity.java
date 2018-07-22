a536454c587a25d1d68da98aa985944af3579cf0
etino7 <tinoet@ymail.com>
2018-07-22 20:50:34 +0200

    v0.1 alpha - Added BottomNavigation

--- app/src/main/java/io/tstud/paperweight/MainActivity.java
+++ app/src/main/java/io/tstud/paperweight/MainActivity.java
@@ -1,25 +1,45 @@
 package io.tstud.paperweight;
 
+
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationViewEx  bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setActionBar();
+        setBottomNavigation();
+
+    }
+
+    private void setActionBar(){
         ActionBar ab = getSupportActionBar();
         ab.setDisplayShowCustomEnabled(true);
         ab.setDisplayShowTitleEnabled(false);
         ab.setCustomView(R.layout.actionbar_slick);
 
         ab.setElevation(0f);
-
     }
 
+    private void setBottomNavigation(){
+        bottomNavigation = (BottomNavigationViewEx)findViewById(R.id.bnve);
+
+        // Animations
+        bottomNavigation.enableAnimation(false);
+        bottomNavigation.enableShiftingMode(false);
+        bottomNavigation.enableItemShiftingMode(false);
+
+        //Text
+        bottomNavigation.setTextVisibility(false);
+    }
 
 }
